71116c7254d6bcf76c2c6c4ae51084caaea3110a
amal <amaldevanands@gmail.com>
2023-10-13 13:31:27 +0530

    Payment Feature added

--- src/utils/userChecker.ts
+++ src/utils/userChecker.ts
@@ -2,6 +2,8 @@
 import {incrementStage,decrementStage,getStage,setStage} from "../utils/state"
 import User from '../models/user.model' 
 import { getReplyMessage } from "../utils/mesaages";
+import { generatePaymentLink } from "../controllers/paymentHandler";
+
 
 export default async function userChecker(incoming: { To?: string; From: any; Body?: string; MediaUrl0?: string; })
 {
@@ -21,7 +23,18 @@ export default async function userChecker(incoming: { To?: string; From: any; Bo
     
     }else{
       if(isExistingUser.credits==0){
-        return `${getReplyMessage('insufficientCredits',0)}.`
+        let paymentLink
+        const paymentLinkParams = {
+          phoneNumber: isExistingUser.phoneNumber.split(':')[1], 
+          amount: 100, 
+          referenceId: `${isExistingUser.phoneNumber}_${new Date().getTime()}`
+        }
+        try{
+          paymentLink = await generatePaymentLink(paymentLinkParams)
+        }catch(e){
+          console.log(`PaymentLink error${e}`)
+        }
+        return `${getReplyMessage('insufficientCredits',0)}. \n ${paymentLink}`
       }
       else
       {
