b67012f26d10d710e9d2a3a4a6db2bfc4dfac3f4
Vinaya <vinayapr23@gmail.com>
2023-10-14 23:39:33 +0530

    aspect ratio

--- src/utils/requestImageGeneration.ts
+++ src/utils/requestImageGeneration.ts
@@ -1,15 +1,46 @@
 import axios from 'axios';
+import sharp from 'sharp';
+
+async function getImageDimensions(mediaUrl: string) {
+  try {
+    // Download the image
+    const response = await axios.get(mediaUrl, { responseType: 'arraybuffer' });
+
+    if (response.config.url) {
+      // Read the image dimensions using sharp
+      let { width, height } = await sharp(response.data).metadata();
+      if(width)
+       width=(Math.floor(width/8))*8
+      if(height)
+        height=(Math.floor(height/8))*8
+      // Delete the temporary file
+     // await unlink(response.config.url);
+      
+
+      return { width, height };
+    } else {
+      console.error('URL is undefined. Cannot delete the temporary file.');
+    }
+  } catch (error) {
+    console.error('Error fetching image or reading dimensions:', error);
+    return null;
+  }
+}
+
+
 
 export default async function makeApiRequest(prompt:string,image:string,userId:string) {
 console.log(`${process.env.Base_URL}/api/image?userId=`+encodeURIComponent(userId))
 console.log(image)
 console.log(userId)
+  const dimensions=await getImageDimensions(image);
+  console.log(dimensions)
   const apiUrl = process.env.API_URL as string;
   const apiKey = process.env.API_KEY as string;
   const requestData = {
     prompt:`${prompt}`,
     webhook_url:`${process.env.Base_URL}/api/image/?userId=${userId}`,
-    image:`https://res.cloudinary.com/dmkarf8ed/image/fetch/w_496,h_496/f_png/${image}`
+    image:`https://res.cloudinary.com/dmkarf8ed/image/fetch/w_${dimensions?.width},h_${dimensions?.height}/f_png/${image}`
    
   };
 
